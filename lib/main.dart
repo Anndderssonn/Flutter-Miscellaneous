5253962d3306320931b93f61e60397ac7c64f7ed
Anderson Valencia <andersson@Anderssons-MacBook-Pro.local>
2026-01-23 20:56:10 -0500

    feat: Add gyroscope sensor

--- lib/main.dart
+++ lib/main.dart
@@ -1,9 +1,12 @@
 import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
 import 'package:miscellaneous/config/config.dart';
 import 'package:miscellaneous/presentation/providers/providers.dart';
 
 void main() {
+  WidgetsFlutterBinding.ensureInitialized();
+  SystemChrome.setPreferredOrientations([DeviceOrientation.portraitUp]);
   runApp(const ProviderScope(child: MainApp()));
 }
 
---
+++ lib/presentation/widgets/shared/main_menu.dart
@@ -0,0 +1,94 @@
+import 'package:flutter/material.dart';
+import 'package:go_router/go_router.dart';
+
+class MenuItems {
+  final String title;
+  final IconData icon;
+  final String route;
+
+  MenuItems({required this.title, required this.icon, required this.route});
+}
+
+final menuItems = <MenuItems>[
+  MenuItems(title: 'Gyroscope', icon: Icons.downloading, route: '/gyroscope'),
+  MenuItems(title: 'Accelerometer', icon: Icons.speed, route: '/accelerometer'),
+  MenuItems(
+    title: 'Magnetometer',
+    icon: Icons.explore_outlined,
+    route: '/magnetometer',
+  ),
+  MenuItems(
+    title: 'Gyroscope Ball',
+    icon: Icons.sports_baseball_outlined,
+    route: '/gyroscope-ball',
+  ),
+  MenuItems(title: 'Compass', icon: Icons.explore, route: '/compass'),
+];
+
+class MainMenu extends StatelessWidget {
+  const MainMenu({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return SliverGrid.count(
+      crossAxisCount: 3,
+      crossAxisSpacing: 10,
+      mainAxisSpacing: 10,
+      children:
+          menuItems
+              .map(
+                (item) => HomeMenuItem(
+                  title: item.title,
+                  route: item.route,
+                  icon: item.icon,
+                ),
+              )
+              .toList(),
+    );
+  }
+}
+
+class HomeMenuItem extends StatelessWidget {
+  final String title;
+  final String route;
+  final IconData icon;
+  final List<Color> bgColors;
+
+  const HomeMenuItem({
+    super.key,
+    required this.title,
+    required this.route,
+    required this.icon,
+    this.bgColors = const [Colors.deepPurpleAccent, Colors.deepPurple],
+  });
+
+  @override
+  Widget build(BuildContext context) {
+    return GestureDetector(
+      onTap: () => context.push(route),
+      child: Container(
+        padding: const EdgeInsets.all(8),
+        decoration: BoxDecoration(
+          borderRadius: BorderRadius.circular(10),
+          gradient: LinearGradient(
+            colors: bgColors,
+            begin: Alignment.topCenter,
+            end: Alignment.bottomCenter,
+          ),
+        ),
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            Icon(icon, color: Colors.white, size: 40),
+            const SizedBox(height: 10),
+            Text(
+              title,
+              textAlign: TextAlign.center,
+              style: const TextStyle(color: Colors.white, fontSize: 12),
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+}
